e919c2fb8dcc1b6c66276e3a6917282b288d846f
santosh <santoshjoy41@gmail.com>
2024-05-21 15:56:50 +0530

    Studentform

---
+++ Studentform.java
@@ -0,0 +1,21 @@
+public class Studentform {
+    public String studentName="sanjay";
+    public String studentId="1234";
+    public String std ="6th";
+    public char section='a';
+    public int age=15;
+    public String fatherName="srinivas";
+
+    public static void main(String[] args) {
+        Studentform sf=new Studentform();
+        System.out.println(sf.studentName);
+        System.out.println(sf.std);
+        System.out.println(sf.section);
+        System.out.println(sf.age);
+        System.out.println(sf.fatherName);
+
+
+    }
+
+
+}
